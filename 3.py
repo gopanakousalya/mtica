920b61bcfdbf42a6c459e3ed2a0fb95c1ff28d4e
gopanakousalya <121160080+gopanakousalya@users.noreply.github.com>
2022-12-24 16:59:36 +0530

    Add files via upload

--- 3.py
+++ 3.py
@@ -1,7 +1,3 @@
-student=[[28,'kousi',79,87],[55,'madhavi',89,99],[1,'dhana',56,98]]
-student.sort()
-print(student)
-student.sort(key=lambda temp:temp[2])
-print(student)
-student.sort(key=lambda temp:temp[3])
-print(student)
+def greet():
+    inpstr=input("enter your name:")
+    print(outstr)
---
+++ functions1.py
@@ -0,0 +1,3 @@
+inpstr=input("enter your name:")
+outstr="good morning"+" "+inpstr
+print(outstr)
